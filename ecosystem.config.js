13bdb18499b7c908fd6d9e89fce63f94ff031670
PabloHernan2000 <phernanmg@gmail.com>
2026-07-09 17:02:24 -0600

    ADD: flujo para seguirmiento de viajes en tiempo real mediante sockets rooms

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: "taxi-hgo-api-dev(3011)",
+            script: "npm",
+            args: "run dev",
+            instances: 1,
+            watch: false
+        }
+    ]
+};
