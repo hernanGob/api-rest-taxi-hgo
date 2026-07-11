29d75a982fd9de7c65904fae183f39f30e51f4a5
PabloHernan2000 <phernanmg@gmail.com>
2026-07-10 18:41:09 -0600

    UPDATE: cluster

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
     apps: [
         {
             name: "taxi-hgo-api-dev-3011",
@@ -6,6 +6,21 @@ module.exports = {
             args: ["run", "start"],
             exec_mode: "fork",
             instances: 1,
+            watch: false
+        }
+    ]
+}; */
+
+module.exports = {
+    apps: [
+        {
+            name: "taxi-hgo-api-dev-3011",
+            script: "npm",
+            args: ["run", "start"],
+            exec_mode: "cluster",
+            max_memory_restart: "700M",
+            autorestart: true,
+            instances: 4,
             watch: false
         }
     ]
